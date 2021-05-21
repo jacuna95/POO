8dfa11bf733d3c5dafe8c8d26eab731464ca9b73
jacuna95 <jose.acuna.sanchez@ucreativa.com>
2021-05-20 20:56:39 -0600

    added main.java, and update gitignore

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,8 +1,12 @@
 package com.ucreativa;
 
+import com.ucreativa.familia.Nacho;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Nacho trabaja= new Nacho();
         System.out.println("Hola Clase");
 
 
---
+++ src/com/ucreativa/familia/Nacho.java
@@ -0,0 +1,7 @@
+package com.ucreativa.familia;
+
+public class Nacho {
+    public Nacho(){
+        System.out.println("Hola Clase");
+    }
+}
